cc13902f9f330c58ef9d7c3f64a2d9162e8d547f
darenlarson <larson.daren@gmail.com>
2019-01-16 16:36:16 -0600

    students table created

--- migrations/20190116152006_CreateLambdaDb.js
+++ migrations/20190116152006_CreateLambdaDb.js
@@ -2,20 +2,21 @@
 exports.up = function(knex, Promise) {
     // make changes to out database
     return knex.schema.createTable('cohorts', function(tbl) {
-        // primary key
-        tbl.increments(); // defaults to a column name of id
+            // primary key
+            tbl.increments(); // defaults to a column name of id
 
-        // other fields
-        tbl.string('name', 255);
+            // other fields
+            tbl.string('name', 255);
 
-        // timestamps
-        tbl.timestamps(true, true);
+            // timestamps
+            tbl.timestamps(true, true);
 
-        // constraints
-        tbl.unique('name', 'uq_cohorts_name');
-    });
+            // constraints
+            tbl.unique('name', 'uq_cohorts_name');
+        })
 };
 
 exports.down = function(knex, Promise) {
   // rollback/undo the changes
+  return knex.schema.dropTableIfExists('cohorts');
 };
